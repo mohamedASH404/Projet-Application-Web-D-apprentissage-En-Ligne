748cb499b5bf1776643f428f959b978328274c0b
aitsidihoumohamed <aitsidihoumohamed@gmail.com>
2023-09-20 15:53:24 +0200

    Ajout de mon projet

--- fonctions.py
+++ fonctions.py
@@ -17,18 +17,30 @@ UPLOAD_FOLDER = './uploads'
 
 def levenshtein_distance(s, t):
     m, n = len(s), len(t)
+
+    # Créer une matrice pour stocker les distances intermédiaires
     d = [[0] * (n + 1) for _ in range(m + 1)]
+
+    # Initialiser la première ligne de la matrice
     for i in range(m + 1):
         d[i][0] = i
+
+    # Initialiser la première colonne de la matrice
     for j in range(n + 1):
         d[0][j] = j
-    for j in range(1, n + 1):
-        for i in range(1, m + 1):
+
+    # Remplir la matrice en utilisant la méthode récursive de Levenshtein
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
             if s[i - 1] == t[j - 1]:
                 substitution_cost = 0
             else:
                 substitution_cost = 1
-            d[i][j] = min(d[i - 1][j] + 1, d[i][j - 1] + 1, d[i - 1][j - 1] + substitution_cost)
+            deletion = d[i - 1][j] + 1
+            insertion = d[i][j - 1] + 1
+            substitution = d[i - 1][j - 1] + substitution_cost
+            d[i][j] = min(deletion, insertion, substitution)
+
     return d[m][n]
 
 
